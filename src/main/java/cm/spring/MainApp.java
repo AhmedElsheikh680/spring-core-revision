c64c84367ef5265610ef9739ca7d3cfc198e1e19
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-05 23:35:18 +0200

    IOC #2

--- src/main/java/cm/spring/MainApp.java
+++ src/main/java/cm/spring/MainApp.java
@@ -1,10 +1,16 @@
 package cm.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
     public static void main(String[] args) {
 
-        Sport sport = new Volleyball();
-        sport.start();
+        ClassPathXmlApplicationContext appCon = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Sport football = appCon.getBean("mySport", Sport.class);
+        football.start();
+
+//        Sport sport = new Volleyball();
+//        sport.start();
 
 //        Football football  = new Football();
 //        football.start();
