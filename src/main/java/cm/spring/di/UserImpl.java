2d93b04c5a3b5fb70701a4850d8cf266b9796090
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-06 09:31:17 +0200

    dependency injection #3

--- src/main/java/cm/spring/di/UserImpl.java
+++ src/main/java/cm/spring/di/UserImpl.java
@@ -4,12 +4,16 @@ public class UserImpl implements User {
 
     private Person person;
 
-    public UserImpl( Person person){
-        this.person = person;
-    }
+//    public UserImpl( Person person){
+//        this.person = person;
+//    }
 
     public void start() {
         System.out.println(" I'm User");
+    }
+
+    public void setPerson(Person person){
+        this.person = person;
     }
 
     public void getPerson(){
