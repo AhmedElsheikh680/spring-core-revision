5b443f123b6f0e1ac0515f96cf6bb6d759480609
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-06 18:06:07 +0200

    @PostConstruct @PreDestroy

--- src/main/java/cm/spring/lifecycle/Admin.java
+++ src/main/java/cm/spring/lifecycle/Admin.java
@@ -3,10 +3,24 @@ package cm.spring.lifecycle;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
+@Scope("singleton")
 public class Admin implements Person{
+
+    @PostConstruct
+    public void initMethod(){
+        System.out.println("Connected To DB");
+    }
+
     public void name() {
         System.out.println("I'm Admin");
+    }
+
+    @PreDestroy
+    public void destroyMethod(){
+        System.out.println("Not Connected To DB");
     }
 }
